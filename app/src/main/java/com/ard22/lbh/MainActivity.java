44406b8ec3d256f72b85b9377d746fbd55cc365c
Amal Rosh <amalrosh707@gmail.com>
2021-10-15 16:16:16 +0530

    Facebook Login changes

--- app/src/main/java/com/ard22/lbh/MainActivity.java
+++ app/src/main/java/com/ard22/lbh/MainActivity.java
@@ -1,14 +1,25 @@
 package com.ard22.lbh;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button toLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toLogin=findViewById(R.id.gotologin);
+        toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
