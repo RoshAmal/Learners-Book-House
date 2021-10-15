64b7256ee473219ca2ff2fc251f9afd2d38adb73
Amal Rosh <amalrosh707@gmail.com>
2021-10-15 15:27:22 +0530

    Facebook Login

--- app/src/main/java/com/ard22/lbh/LoginActivity.java
+++ app/src/main/java/com/ard22/lbh/LoginActivity.java
@@ -1,14 +1,93 @@
 package com.ard22.lbh;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private LoginButton fbLogin;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textView=findViewById(R.id.dynamic_text);
+        fbLogin=(LoginButton) findViewById(R.id.fb_button);
+        callbackManager = CallbackManager.Factory.create();
+        fbLogin.setPermissions(Arrays.asList("email","user_birthday"));
+        fbLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker t = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken == null){
+                textView.setText("");
+                Toast.makeText(LoginActivity.this, "logout", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                loaduserprofile(currentAccessToken);
+            }
+        }
+    };
+    private void loaduserprofile(AccessToken newAccessToken){
+        GraphRequest request=GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(@Nullable JSONObject jsonObject, @Nullable GraphResponse graphResponse) {
+                if(jsonObject!=null){
+                    try {
+                        String email=jsonObject.getString("email");
+                        String id=jsonObject.getString("id");
+                        textView.setText(email);
+                    }
+                    catch(JSONException ex){
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", " email,id");
+        request.setParameters (parameters);
+        request.executeAsync();
     }
 }
